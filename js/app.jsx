80ae85ee8ada82622c1b25781e3347e56cd14fdd
David Nguyen <davidnguyen84@gmail.com>
2015-10-09 16:05:58 +0100

    Start getting HN's Firebase data into the client

--- js/app.jsx
+++ js/app.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Firebase from 'firebase';
+
+const hackerAPI = new Firebase('https://hacker-news.firebaseio.com/v0/topstories');
 
 class App extends React.Component {
+
+  componentDidMount() {
+    let stuff;
+    hackerAPI.once('value', (values) => {
+      stuff = values.val();
+      if (ReactDOM.findDOMNode(this)) {
+        this.setState({ids: stuff});
+      }
+    });
+  }
+
   render() {
+    console.log(this.state);
     return (
       <div>
-        <p>Welcome to the beginnings of Hacker News. Let build.</p>
+        <p>Welcome? to the beginnings of Hacker News. Let build.</p>
       </div>
     );
   }
 }
 
+App.state = {
+  ids: []
+};
 ReactDOM.render(<App />, document.getElementById('app'));
