1b25e43d105eb88cc182bc7e7111fd1b46bfe10f
David Nguyen <davidnguyen84@gmail.com>
2015-10-22 12:25:10 +0100

    Change Firebase to Re-base and add data to state (terrible, i know)

--- js/app.jsx
+++ js/app.jsx
@@ -1,23 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Firebase from 'firebase';
+// import Firebase from 'firebase';
+import Rebase from 're-base';
 
-const hackerAPI = new Firebase('https://hacker-news.firebaseio.com/v0/topstories');
+const HackerAPIBase = Rebase.createClass('https://hacker-news.firebaseio.com/v0/');
+// const HackerAPI = new Firebase('https://hacker-news.firebaseio.com/v0/');
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
   componentWillMount() {
-    let stuff;
-    hackerAPI.once('value', (values) => {
-      stuff = values.val();
-      if (ReactDOM.findDOMNode(this)) {
-        this.setState({ids: stuff});
-      }
+    this.firebaseRef = HackerAPIBase.fetch('topstories', {
+      context: this,
+      asArray: true,
+      then: this.fetchStoryData
+    });
+    // this.firebaseRef = new Firebase('https://hacker-news.firebaseio.com/v0/topstories');
+    // this.firebaseRef.on('value', (dataSnapshot) => {
+    //   this.items = dataSnapshot.val();
+    //   this.setState({
+    //     items: this.items,
+    //   });
+  }
+
+  fetchStoryData(ids) {
+    ids.forEach((id) => {
+      HackerAPIBase.bindToState('item/' + id, {
+        context: this,
+        state: 'storyID_' + id,
+      });
     });
   }
 
+  componentWillUnmount() {
+    HackerAPIBase.removeBinding(this.firebaseRef);
+  }
+
   render() {
-    console.log(this.state);
     return (
       <div>
         <p>Welcome? to the beginnings of Hacker News. Let build.</p>
@@ -26,8 +47,6 @@ class App extends React.Component {
   }
 }
 
-App.state = {
-  ids: []
-};
 
 ReactDOM.render(<App />, document.getElementById('app'));
+   
