d234a45f179a87100427799a98a7898e82c8b8f0
David Nguyen <davidnguyen84@gmail.com>
2016-01-05 20:01:54 -0800

    Move parseUrl function from Story view to store

--- js/components/Story.jsx
+++ js/components/Story.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import url from 'url';
 import moment from 'moment';
 
 export default class Story extends React.Component {
@@ -7,9 +6,6 @@ export default class Story extends React.Component {
     super(props);
   }
 
-  getDomain(link) {
-    return url.parse(link).hostname;
-  }
 
   render() {
     const {content} = this.props;
@@ -18,7 +14,7 @@ export default class Story extends React.Component {
       <li className="hn-StoryListItem">
         <div className="hn-StoryListItem__Subcontainer">
           <a className="hn-StoryListItem__Title" href={content.url}>{content.title}</a>
-          <a className="hn-StoryListItem__Url" href={content.url}>{this.getDomain(content.url)}</a>
+          <a className="hn-StoryListItem__Url" href={content.url}>{content.parsedDomain}</a>
           <span className="hn-StoryListItem__Details">{content.score} points by {content.by} {moment.utc(content.time * 1000).fromNow()} hours ago | {content.kids ? content.kids.length : 0} comments</span>
         </div>
       </li>
--- js/stores/StoryStore.js
+++ js/stores/StoryStore.js
@@ -1,6 +1,7 @@
 import Reflux from 'reflux';
 import $ from 'jquery';
 import StoryActions from '../actions/StoryActions.js';
+import url from 'url';
 
 const StoryStore = Reflux.createStore({
   listenables: [StoryActions],
@@ -18,10 +19,15 @@ const StoryStore = Reflux.createStore({
     });
   },
 
+  parseUrl(link) {
+    return url.parse(link).hostname.replace(/www./, '');
+  },
+
   fetchStoryData(id) {
     const storyUrl = 'https://hacker-news.firebaseio.com/v0/item/' + id + '.json';
 
     $.get(storyUrl, (response) => {
+      response.parsedDomain = this.parseUrl(response.url);
       this.storyData.push(response);
       this.trigger(this.storyData);
     });
