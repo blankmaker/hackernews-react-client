5c81ccf86afae9302dda22313cf98da7d42f7969
David Nguyen <davidnguyen84@gmail.com>
2016-01-11 17:29:53 -0800

    Linting errors

--- webpack.config.js
+++ webpack.config.js
@@ -3,20 +3,20 @@ var Promise = require('es6-promise').Promise;
 
 module.exports = {
   entry: [
-    './js/app.jsx'
+    './js/app.jsx',
   ],
   output: {
     path: __dirname + '/build',
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   module: {
     loaders: [
       { test: /\.jsx?$/, loaders: ['babel'], exclude: /node_modules/ },
       { test: /\.jsx$/, exclude: /node_modules/, loader: 'babel-loader'},
       { test: /\.scss$/, loaders: ['style', 'css', 'sass'] },
-    ]
+    ],
   },
   plugins: [
-    new webpack.NoErrorsPlugin()
-  ]
+    new webpack.NoErrorsPlugin(),
+  ],
 };
